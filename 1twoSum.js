1e10ce46fa85cbdb3733a29d52f522e0b289e263
Jack Knoell <knoelljack@gmail.com>
2022-05-16 21:49:05 -0700

    .

---
+++ 1twoSum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    // let dict = {};
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i]+nums[j] == target) return [i,j];
+        }
+    }
+    return [];
+};
+
+var twoSum = function(nums, target) {
+    let dict = {};
+    for(let i=0;i<nums.length; i++){
+        let diff = target - nums[i];
+        if(diff in dict) return [dict[diff],i]
+        else dict[nums[i]] = i;
+    }
+    return []
+};
