f97ac5d163cb8a97446d6269e85d6756f6096360
Jack Knoell <knoelljack@gmail.com>
2022-06-01 19:48:13 -0700

    .

---
+++ 48rotateImage.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+ var rotate = function(matrix) {
+    for(let i=0; i<matrix.length; i++){
+        for(let j=i; j<matrix[i].length; j++){
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    for(let m of matrix){
+        let first = matrix.shift();
+        matrix.push(first.reverse());
+    }
+};
