5c63eb92c17312c778955efc5e4fc8ed4763e73b
Jack Knoell <knoelljack@gmail.com>
2022-07-26 17:45:08 -0700

    .

--- 46permutations.js
+++ 46permutations.js
@@ -2,30 +2,50 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
- var permute = function(nums) {
-    let res = [];
-    let counter = {};
-    for(let num of nums){
-        counter[num] ? counter[num]++ : counter[num] = 1;
-    }
+//  var permute = function(nums) {
+//     let res = [];
+//     let counter = {};
+//     for(let num of nums){
+//         counter[num] ? counter[num]++ : counter[num] = 1;
+//     }
     
-    var backtrack = function(curr){
-        if(curr.length == nums.length){
-            res.push(curr.slice());
+//     var backtrack = function(curr){
+//         if(curr.length == nums.length){
+//             res.push(curr.slice());
+//             return;
+//         } else {
+//             for(let num in counter){
+//                 if(counter[num] > 0){
+//                     curr.push(num);
+//                     counter[num]--;
+//                     backtrack(curr);
+//                     curr.pop();
+//                     counter[num]++;
+//                 } 
+//             }
+//         }
+//     }
+    
+//     backtrack([]);
+//     return res;
+// };
+
+var permute = function(nums) {
+    let res = [];
+    
+    var dfs = function(i) {
+        if(i == nums.length){
+            res.push(nums.slice());
             return;
-        } else {
-            for(let num in counter){
-                if(counter[num] > 0){
-                    curr.push(num);
-                    counter[num]--;
-                    backtrack(curr);
-                    curr.pop();
-                    counter[num]++;
-                } 
-            }
+        }
+        
+        for(let j=i; j<nums.length; j++){
+            [nums[i],nums[j]] = [nums[j],nums[i]];
+            dfs(i+1);
+            [nums[i],nums[j]] = [nums[j],nums[i]];
         }
     }
     
-    backtrack([]);
+    dfs(0);
     return res;
 };
