aa924ec15c112c6a871d6704889c4a07031ab61a
Jack Knoell <knoelljack@gmail.com>
2022-08-02 17:12:20 -0700

    .

--- 13romanToInteger.js
+++ 13romanToInteger.js
@@ -3,42 +3,19 @@
  * @return {number}
  */
  var romanToInt = function(s) {
-    let res = 0;
-    for(let i=0;i<s.length; i++){
-        if(s[i]=="I"){
-            if(s[i+1]=="V"){
-                res+=4;
-                i++;
-            } else if(s[i+1]=="X"){
-                res+=9;
-                i++;
-            }
-            else res+=1;
-        }
-        else if(s[i]=="V") res+=5;
-        else if(s[i]=="X"){
-            if(s[i+1]=="L"){
-                res+=40;   
-                i++;
-            } else if(s[i+1]=="C"){
-              res+=90;
-                i++;
-            } 
-            else res+=10;
-        }
-        else if(s[i]=="L" && s[i-1] !="X") res+=50;
-        else if(s[i]=="C"){
-            if(s[i+1]=="D"){
-              res+=400;
-                i++;
-            } else if(s[i+1]=="M"){
-              res+=900;
-                i++;
-            } 
-            else res+=100;
-        }
-        else if(s[i]=="D") res+=500;
-        else if(s[i]=="M")res+=1000;
+    let symbols = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000,
     }
-    return res;
+    let sum = 0;
+    for(let i=0; i<s.length; i++){
+        symbols[s[i]] < symbols[s[i+1]] ? sum -= symbols[s[i]] : sum += symbols[s[i]];
+    }
+    
+    return sum;
 };
