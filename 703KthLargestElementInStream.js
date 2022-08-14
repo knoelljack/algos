5bd207f5650ec47002432e2f9194966684d7a3f4
Jack Knoell <knoelljack@gmail.com>
2022-08-13 17:47:13 -0700

    .

---
+++ 703KthLargestElementInStream.js
@@ -0,0 +1,33 @@
+https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+ var KthLargest = function(k, nums) {
+    this.k = k;
+    this.minHeap = new MinPriorityQueue();
+    nums.forEach((num) => {
+        this.add(num);
+    })
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.minHeap.enqueue(val);
+    
+    if(this.minHeap.size() > this.k){
+        this.minHeap.dequeue().element;
+    }
+    
+    return this.minHeap.front().element;
+};
+
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
