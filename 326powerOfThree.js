f14b4c19caa7a233452fee74a336faac8b59e9ca
Jack Knoell <knoelljack@gmail.com>
2022-08-23 18:19:49 -0700

    .

---
+++ 326powerOfThree.js
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/power-of-three/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isPowerOfThree = function(n) {
+    while(n>1){
+        if(n%3 == 0) n /= 3;
+        else break;
+    }
+    return n === 1;
+};
